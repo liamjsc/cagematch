90b63048da38c1d42bbc69b31802f3dcdbbeec3e
liamjsc <ljschauerman@gmail.com>
2020-02-25 23:19:41 -0800

    update color

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,4 +1,4 @@
-export const primary = '#6200EE';
+export const primary = '#552583';
 export const primary_sub = '#3700b3';
 export const secondary = '#03DAC6';
 export const secondary_sub = '#018786';
