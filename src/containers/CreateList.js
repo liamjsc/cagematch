72488fa4040a808b83e2ad8a3d603074e81c2ac2
liamjsc <ljschauerman@gmail.com>
2020-04-04 17:23:07 -0700

    fix dereks bugs

--- src/containers/CreateList.js
+++ src/containers/CreateList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {
+  ActivityIndicator,
   Dimensions,
+  ScrollView,
   StyleSheet,
   TouchableOpacity,
   View,
@@ -19,6 +21,30 @@ const TITLE = 'TITLE';
 const DESCRIPTION = 'DESCRIPTION';
 const ENTRIES = 'ENTRIES';
 
+const initialState = {
+  section: TITLE,
+  title: '',
+  description: '',
+  entries: [],
+  pendingEntry: '',
+  posting: false,
+  posted: false,
+  error: null,
+}
+
+function ListIsPosting({ title }) {
+  return (
+    <View style={styles.posting}>
+      <Text h3>{title}</Text>
+      <Text>Setting up your list for rankings, this might take a moment.</Text>
+      <ActivityIndicator
+        style={{ paddingTop: 25 }}
+        size="large"
+      />
+    </View>
+  )
+}
+
 class CreateList extends Component {
   static navigationOptions = {
     title: 'Create List',
@@ -30,16 +56,7 @@ class CreateList extends Component {
     this.pendingEntryEl = null;
   }
 
-  state = {
-    section: TITLE,
-    title: '',
-    description: '',
-    entries: [],
-    pendingEntry: '',
-    posting: false,
-    posted: false,
-    error: null,
-  }
+  state = initialState;
 
   onClickCreateList = (e) => {
     console.log('click create');
@@ -59,6 +76,7 @@ class CreateList extends Component {
         console.log(newListData);
         const cage = { listId: newListData.list.id, title };
         console.log(cage)
+        this.setState(initialState)
         this.goToCage(cage);
       })
       .catch((error) => {
@@ -103,8 +121,14 @@ class CreateList extends Component {
   }
 
   pushEntry = () => {
+    // validate entry
+    const { pendingEntry } = this.state;
+    const toInsert = pendingEntry.trim();
+    if (!toInsert.length) return false;
+    if (this.state.entries.some(currentEntry => currentEntry === toInsert)) return false;
+
     this.setState({
-      entries: [...this.state.entries, this.state.pendingEntry],
+      entries: [...this.state.entries, toInsert],
       pendingEntry: '',
     });
     this.pendingEntryEl.focus();
@@ -122,130 +146,145 @@ class CreateList extends Component {
     } = this.state; // will be title, description, or entries
 
     if (posting) {
-      return (
-        <View style={styles.posting}>
-          <Text>'posting'</Text>
-        </View>
-      )
+      return <ListIsPosting/>
     }
 
     return (
-      <View style={styles.createList}>
-        <View style={styles.outline}>
-          <View style={[styles.outlineBox, section === TITLE && styles.outlineActive]}>
-            <TouchableOpacity style={styles.touch} onPress={this.goToTitle}>
-              <Text style={styles.outlineText}>Title</Text>
-            </TouchableOpacity>
+      <View style={styles.outer}>
+        <ScrollView style={styles.createList}>
+          <View style={styles.outline}>
+            <View style={[styles.outlineBox, section === TITLE && styles.outlineActive]}>
+              <TouchableOpacity style={styles.touch} onPress={this.goToTitle}>
+                <Text style={styles.outlineText}>Title</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={[styles.outlineBox, section === DESCRIPTION && styles.outlineActive]}>
+              <TouchableOpacity style={styles.touch} onPress={this.goToDescription}>
+                <Text style={styles.outlineText}>Description</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={[styles.outlineBox, section === ENTRIES && styles.outlineActive]}>
+              <TouchableOpacity style={styles.touch} onPress={this.goToEntries}>
+                <Text style={styles.outlineText}>Entries</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <View style={[styles.outlineBox, section === DESCRIPTION && styles.outlineActive]}>
-            <TouchableOpacity style={styles.touch} onPress={this.goToDescription}>
-              <Text style={styles.outlineText}>Description</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={[styles.outlineBox, section === ENTRIES && styles.outlineActive]}>
-            <TouchableOpacity style={styles.touch} onPress={this.goToEntries}>
-              <Text style={styles.outlineText}>Entries</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
 
-        {
-          (section !== TITLE && title) ? (
-            <Text h3>{title}</Text>
-          ) : null
-        }
+          {
+            (section !== TITLE && title) ? (
+              <Text h3>{title}</Text>
+            ) : null
+          }
 
-        <View style={styles.promptBox}>
-          <Text style={styles.promptText}>
-            {this.getPromptText()}
-          </Text>
-        </View>
-
-        { section === TITLE && (
-          <View style={styles.titleInputBox}>
-            <Input
-              label="Title"
-              onChangeText={(text) => {
-                this.setState({ title: text })
-              }}
-              value={title}      
-            />
-            <Button
-              title="Continue"
-              onPress={this.goToDescription}
-              type="clear"
-            />
+          <View style={styles.promptBox}>
+            <Text style={styles.promptText}>
+              {this.getPromptText()}
+            </Text>
           </View>
-        )}
 
-        { section === DESCRIPTION && (
-          <View style={styles.titleInputBox}>
-            <Input
-              label="Description"
-              onChangeText={(text) => {
-                this.setState({ description: text })
-              }}
-              value={description}      
-            />
-            <Button
-              title="Continue"
-              onPress={this.goToEntries}
-              type="clear"
-            />
-          </View>
-        )}
+          { section === TITLE && (
+            <View style={styles.titleInputBox}>
+              <Input
+                label="Title"
+                onChangeText={(text) => {
+                  this.setState({ title: text })
+                }}
+                value={title}      
+              />
+              <Button
+                title="Continue"
+                onPress={this.goToDescription}
+                type="clear"
+              />
+            </View>
+          )}
 
-        { section === ENTRIES && (
-          <View style={styles.titleInputBox}>
-            <Input
-              key="Entries"
-              label="Entries"
-              onChangeText={(text) => {
-                this.setState({ pendingEntry: text })
-              }}
-              onSubmitEditing={this.pushEntry}
-              value={pendingEntry}  
-              ref={(el) => this.pendingEntryEl = el}    
-            />
-            <Button
-              title="Add to list"
-              onPress={this.pushEntry}
-              type="clear"
-            />
-            {
-              entries && entries.length ? (
-                <View>
-                  {
-                    entries.map((entryTitle, i) => {
-                      return (<ListItem
-                        key={i+1}
-                        title={entryTitle}
-                        bottomDivider
-                      />);
-                    })
-                  }
-                </View>
-              ) : <Text>Add at least 3 items</Text>
-            }
-            <Button
-              containerStyle={{marginTop: 50}}
-              title="Save & start ranking"
-              onPress={this.onClickCreateList}
-              disabled={entries.length < 3}
-            />
-          </View>
-        )}
-      </View >
+          { section === DESCRIPTION && (
+            <View style={styles.titleInputBox}>
+              <Input
+                label="Description"
+                onChangeText={(text) => {
+                  this.setState({ description: text })
+                }}
+                value={description}      
+              />
+              <Button
+                title="Continue"
+                onPress={this.goToEntries}
+                type="clear"
+              />
+            </View>
+          )}
+
+          { section === ENTRIES && (
+            <View style={styles.titleInputBox}>
+              <Input
+                key="Entries"
+                label="Entries"
+                onChangeText={(text) => {
+                  this.setState({ pendingEntry: text })
+                }}
+                onSubmitEditing={this.pushEntry}
+                value={pendingEntry}  
+                ref={(el) => this.pendingEntryEl = el}    
+              />
+              <Button
+                title="Add to list"
+                onPress={this.pushEntry}
+                type="clear"
+              />
+            </View>
+          )}
+          { section === ENTRIES && (
+            <View style={styles.entriesArea}>
+              {
+                entries && entries.length ? (
+                  <View style={styles.entriesList}>
+                    {
+                      entries.map((entryTitle, i) => {
+                        return (<ListItem
+                          key={i+1}
+                          title={entryTitle}
+                          bottomDivider
+                        />);
+                      })
+                    }
+                  </View>
+                ) : <Text>Add at least 3 items</Text>
+              }
+              <Button
+                containerStyle={{
+                  marginTop: 10,
+                  marginBottom: 10,
+                }}
+                title="Save & start ranking"
+                onPress={this.onClickCreateList}
+                disabled={entries.length < 3}
+                type="solid"
+              />
+            </View>
+          )}
+          <View style={styles.bottomPadding}/>
+        </ScrollView>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  outer: {
+    // borderWidth: 1,
+    // borderColor: 'green',
+    height: '100%',
+    // paddingBottom: 100,
+  },
   createList: {
     width: '100%',
-    padding: 10,
-    paddingTop: 30,
-    height: '100%',
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginTop: 30,
+    // borderWidth: 1,
+    // borderColor: 'orange',
   },
   outline: {
     flexDirection: 'row',
@@ -257,7 +296,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     // borderWidth: 1,
-    borderColor: 'black',
+    // borderColor: 'black',
     height: 40,
   },
   outlineActive: {
@@ -267,7 +306,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   promptBox: {
-    marginTop: 25,
+    paddingTop: 25,
   },
   promptText: {
     fontSize: 26,
@@ -279,7 +318,21 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
+  entriesList: {
+    // borderWidth: 1,
+    // borderColor: 'orange',
+  },
+  bottomPadding: {
+    height: 30,
+    // borderWidth: 1,
+    // borderColor: 'blue',
+  },
+  posting: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 export default connect()(CreateList);
--- src/util/constants.js
+++ src/util/constants.js
@@ -2,3 +2,7 @@ export const primary = '#552583';
 export const primary_sub = '#3700b3';
 export const secondary = '#03DAC6';
 export const secondary_sub = 'lightblue' || '#018786';
+
+const telegramBlue = '#0088CC';
+const black = '#000000';
+const almostBlack = '#141516';
